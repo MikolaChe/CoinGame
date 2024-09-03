6baf8529b8471d6cca6ea7dfd8d4203efd4aecc0
Mikola_Che <mikola.cherkashin@gmail.com>
2024-09-03 16:12:30 +0500

    I came up with a console game on Kotlin - Coins. The program randomly increases or decreases the number of coins in your wallet with each move. And if the wallet accumulates 20 coins in total, you win - and get a bonus equal to the number of moves. And if the number of coins in the wallet becomes zero - then you lose and become bankrupt.

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+
+fun main() {
+
+    var line: String = "##########";
+    var bonuses = 0
+
+     do {
+         bonuses ++
+
+        val x = Math.random()
+        val y = Math.random()
+        val isBigger = x > y
+
+        if (isBigger) line +="#"
+        if (!isBigger) line = line.drop(1)
+
+        println(line)
+
+        if (line.length == 0) println("GAME OVER - YOU ARE BANKRUPT")
+        if (line.length == 20) println("YOU ARE WIN $bonuses BONUSES")
+
+        Thread.sleep(400)
+    } while (line.length > 0 && line.length < 20)
+
+
+
+
+}
